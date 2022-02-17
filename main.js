4a0c8b593d7d033dcc04953bc90ffd03a107e500
mehm8128 <mehm8128@gmail.com>
2022-02-17 11:06:42 +0900

    日付が変わるタイミングでの時報追加

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { ETwitterStreamEvent, TwitterApi } from "twitter-api-v2"
+import { CronJob } from "cron"
 
 import "dotenv/config"
 // OAuth 1.0a (User context)
@@ -10,13 +11,29 @@ const client = new TwitterApi({
 })
 const client2 = new TwitterApi(process.env.BEARER_TOKEN)
 const stream = await client2.v2.searchStream({
-	"tweet.fields": ["author_id", "created_at", "text"],
+	"tweet.fields": ["id", "author_id", "created_at", "conversation_id", "text"],
 })
 stream.autoReconnect = true
 
 stream.on(ETwitterStreamEvent.Data, async (tweet) => {
 	if (tweet.data.author_id !== "1034716117556592640") {
-		await client.v2.reply("こんにちは！自動返信機能です", tweet.data.id)
+		const tweet2 = await client.v2.singleTweet(tweet.data.id, {
+			"tweet.fields": "conversation_id",
+		}) //conversation_idをtweets/search/recentに使って会話を全部取ってきて、親ツイートの1つ下が自分なら返信しない、もしくは2つ下が自分なら返信しない
+		if (tweet2.conversation_id !== tweet.id) {
+			await client.v2.reply("こんにちは！自動返信機能です", tweet.data.id)
+		}
 		console.log(tweet.data)
 	}
 })
+
+const timeSignal = new CronJob(
+	"00 00 * * *",
+	async () => {
+		const tweet = await client.v2.tweet("日付が変わりました！")
+		console.log(tweet)
+	},
+	null,
+	true,
+	"Asia/Tokyo"
+)
