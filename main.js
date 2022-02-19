781991762d16666962ff35751b893eae5af8cdef
mehm8128 <mehm8128@gmail.com>
2022-02-20 00:17:50 +0900

    改良

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ stream.on(ETwitterStreamEvent.Data, async (tweet) => {
 	if (tweet.data.author_id !== "1034716117556592640") {
 		const tweet2 = await client.v2.singleTweet(tweet.data.id, {
 			"tweet.fields": "conversation_id",
-		}) //conversation_idをtweets/search/recentに使って会話を全部取ってきて、親ツイートの1つ下が自分なら返信しない、もしくは2つ下が自分なら返信しない
-		if (tweet2.conversation_id !== tweet.id) {
-			await client.v2.reply("こんにちは！自動返信機能です", tweet.data.id)
+		}) //TODO:conversation_idをtweets/search/recentに使って会話を全部取ってきて、親ツイートの1つ下が自分なら返信しない、もしくは2つ下が自分なら返信しない
+		if (tweet2.data.text.indexOf("こんにちは")) {
+			await client.v2.reply("こんにちは！！！自動返信機能です", tweet.data.id)
+			console.log(tweet.data)
 		}
-		console.log(tweet.data)
 	}
 })
 
