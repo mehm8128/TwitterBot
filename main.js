4593315aec10d0dfed3c5ec49ce8f35ec71641a8
mehm8128 <mehm8128@gmail.com>
2022-02-22 11:16:10 +0900

    バグ修正

--- main.js
+++ main.js
@@ -17,12 +17,13 @@ stream.autoReconnect = true
 
 stream.on(ETwitterStreamEvent.Data, async (tweet) => {
 	if (tweet.data.author_id !== "1034716117556592640") {
-		const tweet2 = await client.v2.singleTweet(tweet.data.id, {
-			"tweet.fields": "conversation_id",
-		}) //TODO:conversation_idをtweets/search/recentに使って会話を全部取ってきて、親ツイートの1つ下が自分なら返信しない、もしくは2つ下が自分なら返信しない
-		if (tweet2.data.text.indexOf("こんにちは")) {
+		// const tweet2 = await client.v2.singleTweet(tweet.data.id, {
+		// 	"tweet.fields": "conversation_id",
+		// }) //TODO:conversation_idをtweets/search/recentに使って会話を全部取ってきて、親ツイートの1つ下が自分なら返信しない、もしくは2つ下が自分なら返信しない
+		if (tweet.data.text.indexOf("こんにちは") !== -1) {
 			await client.v2.reply("こんにちは！！！自動返信機能です", tweet.data.id)
 			console.log(tweet.data)
+			console.log(tweet.data.text.indexOf("こんにちは"))
 		}
 	}
 })
